6c0023fd53853d569f8d526cfc6d09d645b63c16
unknown <2400032513@kluniversity.in>
2025-11-05 11:50:55 +0530

    commited

--- vite.config.js
+++ vite.config.js
@@ -7,5 +7,5 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
   // Add base path for GitHub Pages deployment (Highly Recommended)
-  base: "./", 
+  base: "", 
 })
